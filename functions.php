1f49245610b044c7eff8fcf42dba867c71fd1b71
Joel <joelmp3@live.com>
2023-07-04 20:52:19 -0600

    changed function name to davelgomoz

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-function apphiver_files()
+function davelgomoz_files()
 {    
     wp_enqueue_script('main-js', get_theme_file_uri('/build/index.js'), array(), '1.0', true);
     //   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
@@ -7,7 +7,7 @@ function apphiver_files()
     wp_enqueue_style('main-styles', get_theme_file_uri('/build/index.css'));
 }
 
-add_action('wp_enqueue_scripts', 'apphiver_files');
+add_action('wp_enqueue_scripts', 'davelgomoz_files');
 
 function post_types()
 {
