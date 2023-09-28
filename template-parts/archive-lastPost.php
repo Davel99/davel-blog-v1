45722f6846831b4456c5b57bdd22ce2996da1abd
Joel <joelmp3@live.com>
2023-09-27 21:43:28 -0600

    Changed message on last post

--- template-parts/archive-lastPost.php
+++ template-parts/archive-lastPost.php
@@ -6,8 +6,8 @@
         <div class="card-body row">
             <h3 class="card-title col-12">Estoy trabajando...</h3>
             <p class="card-text col-12">
-                Voy a subir más contenido conforme pueda.
-                Agradecería mucho que me apoyaras en mis redes sociales.
+                Subiré más contenido conforme pueda.
+                Agradecería mucho que me apoyaras siguiéndome en mis redes sociales.
             </p>
         </div>
     </div>
